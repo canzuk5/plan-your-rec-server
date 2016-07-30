98448aae8fae226d8e9935b7d6d3be778148a516
Matthew Dekker <matt@weaveit.co.nz>
2016-07-30 13:35:46 +1200

    added map markers

--- main.js
+++ main.js
@@ -4,21 +4,26 @@ var path = require('path');
 var dbCon = require('./db.js').getDB();
 var request = require('request');
 var app = express();
+var parseString = require('xml2js').parseString;
 
 app.use('/public', express.static('www'));
 app.use(bodyParser.json({ type: 'application/json', limit: '10mb' }));
 
-app.get(/(^(?!(api.))(?!(public.)))\S+/, function (req, res) {
+app.get(/(^(?!(\/api.))(?!(public.)))\S+/, function (req, res) {
   res.sendFile(path.join(__dirname + '/www/index.html'));
 });
 
-app.get("/api/nodes", function (req, res)){
+app.get("/api/nodes", function (req, res){
 request('http://data.hbrc.govt.nz/Envirodata/EMAR.hts?service=Hilltop&request=SiteList&location=LatLong', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
+    parseString(body, function (err, result) {
+      res.setHeader('Content-Type', "application/json");
+      res.writeHead(200);
+      res.end(JSON.stringify(result.HilltopServer.Site));
+    });
   }
-}
-}
+});
+});
 
 app.listen(80, function () {
   console.log('Example app listening on port 3000!');
--- www/main.js
+++ www/main.js
@@ -1,6 +1,59 @@
 var mymap = L.map('mapid').setView([-39.542321, 176.672763], 10);
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FuenVrIiwiYSI6ImNpcjhlNmltdTAwenRnN20zZzJnaGY2eW0ifQ.V53-PWoukCQNhbBG-cf2tw', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FuenVrIiwiYSI6ImNpcjhlNmltdTAwenRnN20zZzJnaGY2eW0ifQ.V53-PWoukCQNhbBG-cf2tw', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18
 }).addTo(mymap);
+
+populateMarkers();
+
+var data = [];
+
+function populateMarkers(){
+  var cb = function(resultsIn){
+    var resultParsed = JSON.parse(resultsIn);
+    data = [];
+    for (var loc of resultParsed){
+      if (loc.Latitude && loc.Longitude){
+        data.push(loc);
+      var marker = L.marker([loc.Latitude[0], loc.Longitude[0]]).addTo(mymap);
+      marker.bindPopup("<b>" + loc.$.Name + "</b>");
+      marker.on("click", markerClicked)
+    }
+    }
+  }
+  getUrl("/api/nodes", cb);
+}
+
+function markerClicked(e) {
+    var locData = this.getLatLng();
+    for (var loc of data) {
+      if (loc.Latitude[0] == locData.lat){
+        console.log(loc);
+      console.log(locData);
+      console.log("Hit lat.")
+    }
+    if (loc.Longitude[0] == locData.lng){
+      console.log(loc);
+    console.log(locData);
+    console.log("Hit long.")
+  }
+      if (loc.Latitude[0] == locData.lat && loc.Longitude[0] == locData.lng) {
+        console.log($('#selection'));
+        $('#selection').text(loc.$.Name);
+        break;
+      }
+    }
+}
+
+
+function getUrl(url, cb)
+{
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200)
+    cb(req.responseText);
+  }
+  req.open("GET", url, true);
+  req.send(null);
+}
